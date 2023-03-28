d2af76b85015aaba58a7c4be4874c71a46ed7004
Anuj kashyap <anujkashyap123000@gmail.com>
2023-03-28 18:44:36 +0530

    updating the server url

--- client/java.js
+++ client/java.js
@@ -98,7 +98,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   // fetch Data from the server to get response from the bot
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://ai-codex-app-0ibz.onrender.com',{
   method:'POST',
   headers: {
     'Content-Type':'application/json'
